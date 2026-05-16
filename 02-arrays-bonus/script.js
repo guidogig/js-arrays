9cbcf37cf43218fc5de2ccfac10af3b17d38e340
Guido Giglioli <guido.tobugroup@gmail.com>
2026-05-16 10:27:22 +0200

    correzione live a lezione

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -10,17 +10,71 @@ const teachers = [
 
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
-const reversedTeachers = null;
+
+// prima soluzione
+const reversedTeachers = teachers.toReversed();
+
+// seconda soluzione
+const output = [];
+for (let i=0; i<teachers.length; i++) {
+  output.unshift(teachers[i]);
+}
+
+// terza soluzione
+const output2 = [];
+for (let i = teachers.length - 1; i >= 0; i--) {
+  output2.push(teachers[i]);
+}
+
+
+
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
+const out = [];
+for (let i = 0; i < teachers.length; i++) {
+  if (teachers[i].length >= 5) {
+    out.push(teahcers[i]);
+  }
+}
+
+const longNames = out;
 
 // 3. Rimuovi 'Ed' dall'array teachers
+teachers.splice(teachers.indexOf('Ed'), 1);
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
-const isFabioPresent = null;
+let isPresent = false;
+for (let i = 0; i < teachers.length; i++) {
+  if (teachers[i] === 'Fabio') {
+    isPresent = true;
+    break;
+  }
+}
+const isFabioPresent = isPreset;
+
+//OPPURE!
+// const isFabioPresent = teachers.includes('Fabio');
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
-const teachersString = null;
+let out = '';
+for (let i = 0; i < teachers.length; i++) {
+  out += teachers[i];
+  if (i != teachers.length - 1) {
+    out += ',';
+  } else {
+    out += '.';
+  }
+}
+
+
+//OPPURE
+for (let i = 0; i < teachers.length; i++) {
+  out += teachers[i] + (i !== teachers.length -1 ? ',' : '.');
+}
+
+//OPPURE
+out = teachers.join(',');
+
+const teachersString = out;
